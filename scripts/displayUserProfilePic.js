8666a16570fe8618f5dd6331ed4f179af9af52c4
Kevin Baumann <baumannkev@gmail.com>
2021-05-10 19:03:12 -0700

    Added picture

--- scripts/displayUserProfilePic.js
+++ scripts/displayUserProfilePic.js
@@ -1,14 +1,12 @@
 //After a user uploads a profile picture, display it on the profile page.
-function displayUserProfilePic() {
-    var storage = firebase.storage();
-    var storageRef = storage.storageRef();
+// function displayUserProfilePic() {
+//     var storage = firebase.storage();
+//     var storageRef = storage.storageRef();
 
-    var imagesRef = storageRef.child('pictures')
+//     var imagesRef = storageRef.child('pictures')
 
-    var backRef = storageRef.child('pictures/background-image.jpg');
-
-                    if (backRef != null) {
-                        $("#background").append("<img src='" + backRef + "' id='photo1'>")
-                    }
-}
-displayUserProfilePic();
+//     var backRef = storageRef.child('pictures/background-image.jpg');
+    
+//     $("#background").append("<img src='" + backRef + "' id='photo1'>")
+// }
+// displayUserProfilePic();
