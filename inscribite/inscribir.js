a36f36023de1b831c07cd602f48f036fe980d08b
Kevin Baumann <baumannkev@gmail.com>
2022-03-15 22:52:25 -0700

    edited calendar

--- inscribite/inscribir.js
+++ inscribite/inscribir.js
@@ -1,15 +1,25 @@
 var today = new Date();
 
+
 // $('#spanish_calendar').calendar('disableMinute', ('true'))
 $('#spanish_calendar')
     .calendar({
+        // today: "true",
+        inline: "true",
+        closable: "false",
+        onChange: function changeDateSelected() {
+            let inputVal = document.getElementById("inputID").value;
+            console.log(inputVal)
+            $('#dateSelected').text(inputVal);
+        },
+        type: 'date',
         on: "hover",
-        disableMinute: "true",
+        // disableMinute: "true",
         minDate: new Date(today.getFullYear(), today.getMonth(), today.getDate()),
         maxDate: new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30),
         text: {
             days: ['D', 'L', 'M', 'M', 'J', 'V', 'S'],
-            months: ['Enero', 'Febrero', 'Marso', 'Avril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
+            months: ['Enero', 'Febrero', 'Marzo', 'Avril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
             monthsShort: ['Ene', 'Feb', 'Mar', 'Avr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'],
             today: 'Hoy',
             now: 'Ahora',
